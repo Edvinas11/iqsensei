ec41eff5c1f87739549c7c96033c3ad9ff26163b
rafalero <rapolas.jonas.labutis@gmail.com>
2023-05-23 23:28:57 +0300

    pradetas post req handlinimas

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.user_list)
+    path('users', views.user_list),
+    path('newuser', views.new_user),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,4 @@
+import json
 from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework import generics, status
@@ -5,9 +6,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from api.models import User
 from api.serializers import UserSerializer
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 
+@csrf_exempt
 def user_list(request):
     if request.method == 'GET':
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+@csrf_exempt
+@api_view(['POST'])
+def new_user(request):
+    if request.method == 'POST':
+        # Nebaigta
+        return JsonResponse({"Kebabas" : "Cesnakinis"}, status=201)
