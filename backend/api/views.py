8f4c9dcb97b83fb1935845e2743f82f49a5ed227
rafalero <rapolas.jonas.labutis@gmail.com>
2023-05-23 22:04:39 +0300

    user list api response on /users

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from api.models import User
+from api.serializers import UserSerializer
 
-# Create your views here.
+def user_list(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return JsonResponse(serializer.data, safe=False)
