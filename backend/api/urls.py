95c1a2c59b4ef2547cd1e13407ab62187427fdf2
rafalero <rapolas.jonas.labutis@gmail.com>
2023-07-02 15:51:49 +0300

    JWT Token setup in the backend, frontend requires cookie fix

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('register', views.UserRegister.as_view()),
@@ -7,4 +8,9 @@ urlpatterns = [
     path('logout', views.UserLogout.as_view()),
     path('user', views.UserView.as_view()),
     path('authenticated', views.UserCheckAuthenticated.as_view()),
+    
+    path('token', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('home', views.HomeView.as_view(), name="home"),
+    path('logout', views.LogoutView.as_view(), name="logout")
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import get_user_model, login, logout
-from rest_framework.authentication import SessionAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
 from rest_framework import permissions, status
 from .validations import custom_validation, validate_email, validate_password
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
+# ======================= CURRENTLY DISABLED ====================
 class UserCheckAuthenticated(APIView):
     permission_classes = (permissions.AllowAny,)
 
@@ -59,8 +61,28 @@ class UserLogout(APIView):
 
 class UserView(APIView):
 	permission_classes = (permissions.IsAuthenticated,)
-	authentication_classes = (SessionAuthentication,)
 	##
 	def get(self, request):
 		serializer = UserSerializer(request.user)
 		return Response({'user': serializer.data}, status=status.HTTP_200_OK)
+
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ CURRENTLY DISABLED ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+class HomeView(APIView):
+    
+	permission_classes = (IsAuthenticated, )
+	def get(self, request):
+		content = {'message': 'Welcome to the JWT Authentication page using React Js and Django!'}
+		return Response(content)
+
+class LogoutView(APIView):
+     permission_classes = (IsAuthenticated,)
+     def post(self, request):
+          
+          try:
+               refresh_token = request.data["refresh_token"]
+               token = RefreshToken(refresh_token)
+               token.blacklist()
+               return Response(status=status.HTTP_205_RESET_CONTENT)
+          except Exception as e:
+               return Response(status=status.HTTP_400_BAD_REQUEST)
