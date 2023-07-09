efb9fc0c0adaa1d34a8e8930c91b743b79f11b6a
Edvinas11 <edvinasty@gmail.com>
2023-07-09 10:54:59 +0200

    Added authorization header to logout

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -7,179 +7,175 @@ import {
   AUTHENTICATED_FAIL,
   AUTHENTICATED_SUCCESS,
   LOGOUT_SUCCESS,
-  LOGOUT_FAIL
-} from './types';
+  LOGOUT_FAIL,
+} from "./types";
 
-export const updateToken = () => async dispatch => {
+export const updateToken = () => async (dispatch) => {
   const config = {
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
     withCredentials: true,
   };
 
-  const body = JSON.stringify({ refresh_token: sessionStorage.getItem('refresh_token') });
-}
-
+  const body = JSON.stringify({
+    refresh_token: sessionStorage.getItem("refresh_token"),
+  });
+};
 
 export const checkAuthenticated = () => async (dispatch) => {
   const config = {
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
     withCredentials: true,
-  }
-
-<<<<<<< HEAD
-  const body = JSON.stringify({ refresh:sessionStorage.getItem('refresh_token') });
-=======
+  };
 
   // Checking whether user has refresh token at all to not send unnecessary requests to the backend
   var refreshToken = sessionStorage.getItem("refresh_token");
-  if(refreshToken == null){
+  if (refreshToken == null) {
     dispatch({
       type: AUTHENTICATED_FAIL,
-      payload: false
-    })
+      payload: false,
+    });
     return;
   }
 
-
-  const body = JSON.stringify({ refresh:refreshToken });
-
->>>>>>> 79b8fd58fc8e261bb9549d7bcddb215db8b377e2
+  const body = JSON.stringify({ refresh: refreshToken });
 
   try {
     const response = await axios.post(
       `${import.meta.env.VITE_APP_API_URL}/api/token/refresh`,
       body,
       config
-      );
+    );
 
-      if(response.status === 200){
-        sessionStorage.setItem("access_token", response.data.access);
+    if (response.status === 200) {
+      sessionStorage.setItem("access_token", response.data.access);
 
-        dispatch({
-          type: AUTHENTICATED_SUCCESS,
-          payload: true
-        })
-      }
-    } catch (error) {
       dispatch({
-        type: AUTHENTICATED_FAIL,
-        payload: false
-    })
+        type: AUTHENTICATED_SUCCESS,
+        payload: true,
+      });
+    }
+  } catch (error) {
+    dispatch({
+      type: AUTHENTICATED_FAIL,
+      payload: false,
+    });
   }
 };
 
 export const register = (username, password, email) => async (dispatch) => {
   const config = {
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
     withCredentials: true,
   };
-  
+
   const body = JSON.stringify({ username, password, email });
-  
+
   try {
     const response = await axios.post(
       `${import.meta.env.VITE_APP_API_URL}/api/register`,
       body,
       config
-      );
-      
-      if (response.status === 201) {
-        sessionStorage.setItem("access_token", response.data.access_token);
-        sessionStorage.setItem("refresh_token", response.data.refresh_token);
-        
-        dispatch({
-          type: REGISTER_SUCCESS,
-        });
-      } else {
-        dispatch({
-          type: REGISTER_FAIL,
-        });
-      }
-    } catch (error) {
+    );
+
+    if (response.status === 201) {
+      sessionStorage.setItem("access_token", response.data.access_token);
+      sessionStorage.setItem("refresh_token", response.data.refresh_token);
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+      });
+    } else {
       dispatch({
         type: REGISTER_FAIL,
       });
     }
+  } catch (error) {
+    dispatch({
+      type: REGISTER_FAIL,
+    });
+  }
+};
+
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
   };
-  
-  export const login = (email, password) => async (dispatch) => {
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      withCredentials: true,
-    };
-    
-    const body = JSON.stringify({ email, password });
-    
+
+  const body = JSON.stringify({ email, password });
+
   try {
     const response = await axios.post(
       `${import.meta.env.VITE_APP_API_URL}/api/login`,
       body,
       config
-      );
-      
-      if (response.status === 200) {
-        sessionStorage.setItem("access_token", response.data.access_token);
-        sessionStorage.setItem("refresh_token", response.data.refresh_token);
-        
-        // Set the Authorization header for future API requests
-        axios.defaults.headers.common[
-          "Authorization"
-        ] = `Bearer ${response.data.access_token}`;
-        
-        dispatch({
-          type: LOGIN_SUCCESS,
-        });
-      } else {
-        dispatch({
-          type: LOGIN_FAIL,
-        });
-      }
-    } catch (error) {
-      // Handle the error
-      console.error(error);
-    }
-  };
+    );
 
-  export const logout = () => async dispatch => {
-    const config = {
-      headers: {
-        'Accept': 'application/json',
-        "Content-Type": "application/json"
-      },
-      withCredentials: true
-    };
-    
-  
-    const body = JSON.stringify({ refresh_token: sessionStorage.getItem('refresh_token') });
-  
-    try {
-      const response = await axios.post(
-        `${import.meta.env.VITE_APP_API_URL}/api/logout`,
-        body,
-        config
-      );
-      if(response.status === 205){
-        sessionStorage.removeItem("access_token");
-        sessionStorage.removeItem("refresh_token");
-  
-        dispatch({
-          type: LOGOUT_SUCCESS
-        });
-      }
-    } catch (error) {
+    if (response.status === 200) {
+      sessionStorage.setItem("access_token", response.data.access_token);
+      sessionStorage.setItem("refresh_token", response.data.refresh_token);
+
+      // Set the Authorization header for future API requests
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${response.data.access_token}`;
+
       dispatch({
-        type: LOGOUT_FAIL
+        type: LOGIN_SUCCESS,
+      });
+    } else {
+      dispatch({
+        type: LOGIN_FAIL,
       });
     }
+  } catch (error) {
+    // Handle the error
+    console.error(error);
   }
+};
+
+export const logout = () => async (dispatch) => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Authorization":`Bearer ${sessionStorage.getItem('access_token')}`
+    },
+    withCredentials: true,
+  };
+
+  const body = JSON.stringify({
+    refresh_token: sessionStorage.getItem("refresh_token"),
+  });
+
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_APP_API_URL}/api/logout`,
+      body,
+      config
+    );
+    if (response.status === 205) {
+      sessionStorage.removeItem("access_token");
+      sessionStorage.removeItem("refresh_token");
+
+      dispatch({
+        type: LOGOUT_SUCCESS,
+      });
+    }
+  } catch (error) {
+    dispatch({
+      type: LOGOUT_FAIL,
+    });
+  }
+};
