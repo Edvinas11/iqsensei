1f52939453d1b7117116f3529dbe8da6610cd0a5
Edvinas11 <edvinasty@gmail.com>
2023-07-09 18:08:36 +0200

    Naming

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -98,7 +98,7 @@ const Login = ({ isAuthenticated, login }) => {
                       <div role="status">
                         <svg
                           aria-hidden="true"
-                          class="inline w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-300 fill-gray-100"
+                          className="inline w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-300 fill-gray-100"
                           viewBox="0 0 100 101"
                           fill="none"
                           xmlns="http://www.w3.org/2000/svg"
