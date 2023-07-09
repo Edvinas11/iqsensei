4cc6c1b9fdf5715431956b8df732a1461ef50cfe
Edvinas11 <edvinasty@gmail.com>
2023-07-09 17:55:45 +0200

    Added error message and loading icon

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ bgColor, color, size, text, borderRadius, styles, type }) => {
+const Button = ({ bgColor, color, size, text, borderRadius, styles, type, disabled }) => {
   return (
-    <button type={type} style={{ backgroundColor: bgColor, color, borderRadius }} className={`text-${size} font-poppins font-semibold p-3 hover:drop-shadow-xl ${styles}`}>
+    <button disabled={disabled} type={type} style={{ backgroundColor: bgColor, color, borderRadius }} className={`text-${size} font-poppins font-semibold p-3 hover:drop-shadow-xl ${styles}`}>
       {text}
     </button>
   )
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import Button from "./Button";
 import Hero from "./Hero";
+import ErrorMessage from "./ErrorMessage";
 
 export {
     Navbar,
     Button,
     Hero,
+    ErrorMessage,
 }
